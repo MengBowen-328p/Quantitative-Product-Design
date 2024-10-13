a75c22f8224854b4a2d961f7fb70dd6cfbe36dba
MengBowen-328p <mbw.mega328p@gmail.com>
2024-10-13 19:13:31 +0800

    Update Work7

--- Work1/demo3.m
+++ Work1/demo3.m
@@ -1,29 +1,45 @@
-% t=-1:0.01:5;
-% f1=((t>0)-(t>3));
-% f2=exp(-t).*(t>0);
-% f3=conv(f1,f2);f3=f3*0.01;
-% k=2*length(t)-1;
-% k3=linspace(2*t(1),2*t(end),k);
-% subplot(2,2,1)
-% plot(t,f1);
-% title('f1(t)')
-% xlabel('t')
-% ylabel('f1(t)')
-% subplot(2,2,2)
-% plot(t,f2)
-% title('f2(t)')
-% xlabel('t')
-% ylabel('f2(t)')
-% subplot(2,2,3)
-% plot(k3,f3);
-% h=get(gca,'position');
-% h(3)=2.5*h(3);
-% set(gca,'position',h);
-% title('f(t)=f1(t)*f2(t)');
-% xlabel('t');
-% ylabel('f(t)');
-g_signal_t = 0:1/50:10-1/50
-k = 2*length(g_signal_t)-1;
-b = linspace(2*g_signal_t(1),g_signal_t(end),k);
-b
+% 设置参数
 
+Fs = 1000;  % 采样率
+
+Fc = 100;   % 截止频率
+
+N = 60;  % 滤波器的阶数
+
+T = 1/Fs;  % 采样周期
+
+L = 1000;  % 信号长度
+
+t = (0:L-1)*T;  % 时间向量
+
+% 生成包含噪声的信号
+
+S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
+
+X = S + 2*randn(size(t));
+
+% 设计滤波器
+
+win = hann(N+1);  % 汉宁窗
+
+b = fir1(N, Fc/(Fs/2), 'low', win);
+
+% 对信号进行滤波
+
+Y = filter(b, 1, X);
+
+% 绘制原始信号和滤波后的信号
+
+figure;
+
+subplot(2,1,1);
+
+plot(Fs*t(1:50), X(1:50))
+
+title('原始信号');
+
+subplot(2,1,2);
+
+plot(Fs*t(1:50), Y(1:50))
+
+title('滤波后的信号');
